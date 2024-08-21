3f4802edd8210dfd808145a20a200c4e8c3f190c
Johannes Sappl <johannes.sappl@uibk.ac.at>
2024-08-21 15:10:29 +0200

    refactor: init model from channels list

--- uibk/deep_preconditioning/model.py
+++ uibk/deep_preconditioning/model.py
@@ -10,25 +10,33 @@ from torch import nn
 
 
 class PreconditionerNet(nn.Module):
-    """CNN returns preconditioner for conjugate gradient solver."""
+    """Fully convolutional network mapping matrices to lower triangular matrices."""
 
-    def __init__(self) -> None:
-        """Initialize the network architecture."""
+    def __init__(self, channels: list[int]) -> None:
+        """Initialize the network architecture.
+
+        Args:
+            channels: Even (mandatory because padding) number of channels in all layers.
+        """
         super().__init__()
 
+        assert len(channels) % 2
+
+        # input layer
         self.layers = spconv.SparseSequential(
-            spconv.SparseConv2d(1, 64, 1),
+            spconv.SparseConv2d(channels[0], channels[1], 1),
             nn.PReLU(),
-            spconv.SparseConv2d(64, 256, 2, padding=(1, 0)),
-            nn.PReLU(),
-            spconv.SparseConv2d(256, 512, 2, padding=(1, 0)),
-            nn.PReLU(),
-            spconv.SparseConv2d(512, 256, 2, padding=(0, 1)),
-            nn.PReLU(),
-            spconv.SparseConv2d(256, 64, 2, padding=(0, 1)),
-            nn.PReLU(),
-            spconv.SparseConv2d(64, 1, 1),
         )
+
+        # hidden layers
+        for index, (in_channels, out_channels) in enumerate(zip(channels[1:-2], channels[2:-1], strict=True)):
+            padding = (1, 0) if index < (len(channels) - 2) // 2 else (0, 1)
+
+            self.layers.add(spconv.SparseConv2d(in_channels, out_channels, 2, padding=padding))
+            self.layers.add(nn.PReLU())
+
+        # output layer
+        self.layers.add(spconv.SparseConv2d(channels[-2], channels[-1], 1))
 
     def forward(self, input_: spconv.SparseConvTensor) -> spconv.SparseConvTensor:
         """Return the `L` part of the `L @ L.T` preconditioner for the conjugate gradient solver.
@@ -42,8 +50,9 @@ class PreconditionerNet(nn.Module):
         interim = self.layers(input_)
 
         (filter, ) = torch.where(interim.indices[:, 1] < interim.indices[:, 2])  # (batch, row, col)
-        interim.features[filter] = 0  # make the matrix lower triangular
+        interim.features[filter] *= 0  # make the matrix lower triangular
 
-        # TODO: Check diagonal, maybe enforce positive values?
+        (filter, ) = torch.where(interim.indices[:, 1] == interim.indices[:, 2])
+        interim.features[filter] = nn.functional.softplus(interim.features[filter])  # enforce positive diagonal
 
         return interim
